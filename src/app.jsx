e109b9d55b184b3b37f66b270d9c6fa93e2c5272
namboram <lkjand00@gmail.com>
2021-02-22 20:26:25 +0900

    API KEY 숨기기

--- src/app.jsx
+++ src/app.jsx
@@ -3,33 +3,17 @@ import './app.css';
 import Header from './components/header/header';
 import VideoList from './components/video_list/video_list';
 
-function App() {
+function App({youtubeFetch}) {
 const [videos , setVideos] = useState([]);
 
 const search = (query) => {
-  const requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
-  
-  fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyDPaWCVJ600ulq-FjMm85WkcBbTAb026BU`, requestOptions)
-    .then(response => response.json())
-    .then(result => result.items.map(item => ({...item, id:item.id.videoId})))
-    .then(items => setVideos(items))
-    .catch(error => console.log('error', error));
-
+  youtubeFetch.search(query)
+  .then(videos => setVideos(videos));
 }
 
 useEffect(()=>{
-  const requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
-  
-  fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDPaWCVJ600ulq-FjMm85WkcBbTAb026BU", requestOptions)
-    .then(response => response.json())
-    .then(result => setVideos(result.items))
-    .catch(error => console.log('error', error));
+  youtubeFetch.mostPopular()
+  .then(videos => setVideos(videos));
 }, []);
 
   
--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
+import YoutubeFetch from './service/youtubeFetch';
+
+const youtubeFetch = new YoutubeFetch(process.env.REACT_APP_YOUTUBE_API_KEY);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App youtubeFetch={youtubeFetch}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
